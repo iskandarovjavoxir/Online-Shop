590756916197887e461eb1a888fa16a2bc15b109
Iskandarov Javoxir <origamingjavoxir@gmail.com>
2024-12-10 21:09:31 +0500

    Updated

--- src/main/java/org/example/onlineshop/model/Card.java
+++ src/main/java/org/example/onlineshop/model/Card.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Date;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
@@ -15,4 +17,6 @@ public class Card {
     private Long id;
     @Column(nullable = false, unique = true)
     private String card_number;
+    @Column(nullable = false)
+    private Date expiry_date;
 }
